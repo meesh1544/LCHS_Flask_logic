c49dbb7e4eab4e313010289a909d89ef1e1514b5
meesh1544 <77475637+meesh1544@users.noreply.github.com>
2024-04-13 22:43:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,50 @@
-from flask import Flask, render_template, request, redirect
-import string
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-def check_inputs(name, pwd, check, form_info):
-    # Check the submitted username, password, and retyped password.
-    valid_name = check_username(name, form_info)
-    valid_password = check_password(pwd, form_info)
-    valid_match = valid_password and check_confirm(check, pwd, form_info)
+images = ['grid_0.png', 'grid_1.png', 'grid_2.png', 'grid_3.png', 'grid_4.png',
+    'grid_5.png', 'grid_6.png', 'grid_7.png', 'grid_8.png']
 
-    # Return False if any of the checks fail.
-    return valid_name and valid_password and valid_match
-
-def check_username(name, form_info):
-    return 3 <= len(name) <= 8 and ' ' not in name # Return False if incorrect length, or if spaces are present.
-
-def check_password(pwd, form_info):
-    special_char = letter = digit = False
-
-    # Return False if incorrect length, or if spaces are present.
-    if len(pwd) < 8 or ' ' in pwd:
-        return False
-
-    # Check if pwd contains a letter, number, and special symbol.
-    for char in pwd:
-        if char in string.ascii_letters:
-            letter = True
-        if char in string.digits:
-            digit = True
-        if char in ['%', '#', '&', '*']:
-            special_char = True
-    
-    # Return False if missing any of the three.
-    return letter and digit and special_char
-
-def check_confirm(check, pwd, form_info):
-    return check == pwd     # Return False if the retyped password doesn't match the first.
+def fill_choices(index):
+    options = {}
+    if index in [1, 2, 4, 5, 7, 8]:
+        options['Left'] = index-1
+    elif index == 3:
+        options['Left'] = 5
+    if index in [0, 1, 3, 4, 6, 7]:
+        options['Right'] = index+1
+    elif index == 5:
+        options['Right'] = 3
+    if index in [1, 3, 4, 5, 6, 7, 8]:
+        options['Up'] = (index-3)%9
+    if index in [0, 1, 2, 3, 4, 5, 7]:
+        options['Down'] = (index+3)%9
+    return options
 
 @app.route('/', methods=['GET', 'POST'])
-def sign_up():
-    inputs = {
-        # Label: [type, name, placeholder]
-        'Username': ['text', 'username', '3-8 characters, no spaces'],
-        'Password': ['password', 'password','8 or more characters, no spaces'],
-        'Confirm Password': ['password', 'confirm', 'Retype password']
-    }
-    if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
-        confirm = request.form['confirm']
+def show_grid():
+    if request.method == 'POST':
+        # When the user submits the form, the value of the selected radio
+        # button identifies the new box to highlight in the grid.
+        choice = int(request.form['choice'])    # Collect the radio button value from the form.
+        old_steps = request.form['steps']       # Retrieve the old 'steps' string from the webpage. 
+        steps = f"{old_steps}-{choice}"     # Generate a new 'steps' string.
+        image = images[choice]              # Assign a file name from the images list.
+        choices = fill_choices(choice)      # Call the fill_choices() function, which returns a dictionary of the allowed directions and values for the new box.
+    else:
+        # Randomly select a starting grid image.
+        image = random.choice(images)
+        # Determine which direction choices belong with the highlighted box.
+        choices = fill_choices(images.index(image))
+        # Assign the number of the highlighted box to the 'steps' string.
+        steps = str(images.index(image))
 
-        # If all of the input fields contain valid data, send the user to the success page.
-        if check_inputs(username, password, confirm, inputs):
-            return redirect('/success')
-
-    tab_title = "Flask Project"
-    page_title = "Improve Form UX"
-    return render_template('register.html', tab_title=tab_title, page_title=page_title,
-        inputs=inputs)
-
-@app.route('/success', methods=['GET', 'POST'])
-def success():
-    tab_title = "Flask Project"
-    page_title = "Success!"
-    return render_template('success.html', tab_title=tab_title, page_title=page_title)
+    tab_title = "Flask Exercises"
+    page_title = "Move Around A Grid"
+    return render_template('grid.html', tab_title=tab_title, page_title=page_title,
+        choices=choices, image=image, steps=steps)
 
 if __name__ == '__main__':
     app.run()
